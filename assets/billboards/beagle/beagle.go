e4821fd229ceb8cf513785af5d322f6adadc1aca
Aaron Leung <modus.tollens@gmail.com>
2025-05-31 01:32:48 +0200

    [WIP] get billboards working

---
+++ assets/billboards/beagle/beagle.go
@@ -0,0 +1,31 @@
+components {
+  id: "beagle"
+  component: "/environment/billboards/beagle/beagle.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle_toward\"\n"
+  "material: \"/materials/geometry/sprite-instanced.material\"\n"
+  "size {\n"
+  "  x: 23.5\n"
+  "  y: 15.75\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"diffuse_map\"\n"
+  "  texture: \"/assets/billboards/beagle/beagle.tilesource\"\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"normal_map\"\n"
+  "  texture: \"/assets/billboards/flat.tilesource\"\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"spec_glow_map\"\n"
+  "  texture: \"/assets/billboards/flat.tilesource\"\n"
+  "}\n"
+  ""
+  position {
+    y: 3.0
+  }
+}
