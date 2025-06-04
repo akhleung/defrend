7f195e90a16653baf4852d86216d1990af060484
Aaron Leung <modus.tollens@gmail.com>
2025-06-04 02:35:44 +0200

    get instanced, scalable static billboards working (sprites have to be batched in Defold); move some stuff around

---
+++ assets/billboards/flowerbed/ground.go
@@ -0,0 +1,192 @@
+components {
+  id: "ground0"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 0.5
+    z: -1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground1"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 1.5
+    z: -1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground2"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 0.5
+    z: -0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground3"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 1.5
+    z: -0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground4"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 0.5
+    z: 0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground5"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 1.5
+    z: 0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground6"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 0.5
+    z: 1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground7"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: 1.5
+    z: 1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground8"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -1.5
+    z: -0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground9"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -1.5
+    z: 0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground10"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -1.5
+    z: 1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground11"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -0.5
+    z: 0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground12"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -0.5
+    z: -1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground13"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -0.5
+    z: 1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground14"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -0.5
+    z: -0.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "ground15"
+  component: "/assets/billboards/flowerbed/ground.model"
+  position {
+    x: -1.5
+    z: -1.5
+  }
+  rotation {
+    x: -0.70710677
+    w: 0.70710677
+  }
+}
