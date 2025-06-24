a08e66c1831358515af332cdb187514e3822836a
Aaron Leung <modus.tollens@gmail.com>
2025-06-24 16:33:09 +0200

    create GOs for point lights and decals; relocate stuff

---
+++ defrend/assets/decals/static_decal_prototype.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/cube.dae\"\n"
+  "name: \"{{NAME}}\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/defrend/materials/geometry/decal/decal.material\"\n"
+  "}\n"
+  ""
+}
