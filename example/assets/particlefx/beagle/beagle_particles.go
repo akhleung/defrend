8d3a971c978e93eafcf1c76f7dc13b0a7930e934
Aaron Leung <modus.tollens@gmail.com>
2025-11-01 23:15:41 -0700

    add a dithered particle system to the example scene

---
+++ example/assets/particlefx/beagle/beagle_particles.go
@@ -0,0 +1,8 @@
+components {
+  id: "beagle_particles"
+  component: "/example/assets/particlefx/beagle/beagle_particles.script"
+}
+components {
+  id: "beagle_emitter"
+  component: "/example/assets/particlefx/beagle/beagle_emitter.particlefx"
+}
