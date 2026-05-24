12f243901c7b65827f3ba3bc0566151672643f4e
Aaron Leung <modus.tollens@gmail.com>
2026-05-24 05:21:40 -0700

    add new particle fx example

---
+++ example/assets/billboards/dust/dust.go
@@ -0,0 +1,8 @@
+components {
+  id: "dust"
+  component: "/example/assets/billboards/dust/dust.particlefx"
+}
+components {
+  id: "dust_particles"
+  component: "/example/assets/billboards/dust/dust_particles.script"
+}
